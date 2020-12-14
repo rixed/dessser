9c53d13dcf808800b6a2ece1961da936a7b99e52
Cedric Cellier <rixed@happyleptic.org>
2020-12-14 11:09:28 +0100

    C++ backend: add implementation for dessserc aggregator command

--- src/DessserDSTools_FragmentsCPP.ml
+++ src/DessserDSTools_FragmentsCPP.ml
@@ -1,4 +1,5 @@
 let converter ?(out_buf_size=50_000) entry_point =
+let readWholeFile =
   Printf.sprintf {|
 static std::string readWholeFile(std::string const fname)
 {
@@ -8,6 +9,11 @@ static std::string readWholeFile(std::string const fname)
   return str;
 }
 
+|}
+
+let converter ?(out_buf_size=50_000) entry_point =
+  readWholeFile ^
+  Printf.sprintf {|
 int main(int numArgs, char **args)
 {
   char const *fname = "/dev/stdin";
@@ -65,14 +71,61 @@ int main(int numArgs, char **args)
 }
 |} out_buf_size entry_point
 
-let dumper ?(out_buf_size=50_000) _convert_key_id _convert_val_id =
+let dumper ?(out_buf_size=50_000) _convert_key_name _convert_val_name =
   ignore out_buf_size ;
   DessserTools.todo "dumper for C++"
 
-let loader ?(out_buf_size=50_000) _convert_key_id _convert_val_id =
+let loader ?(out_buf_size=50_000) _convert_key_name _convert_val_name =
   ignore out_buf_size ;
   DessserTools.todo "loader for C++"
 
-let aggregator ?(out_buf_size=50_000) _state_id _input_id _output_id =
-  ignore out_buf_size ;
-  DessserTools.todo "aggregator for C++"
+let aggregator ?(out_buf_size=50_000) _state_name input_name output_name =
+  readWholeFile ^
+  Printf.sprintf {|
+int main(int numArgs, char **args)
+{
+  char const *fname = "/dev/stdin";
+  char delim = '\n';  // added after each output
+
+  for (int a = 1; a < numArgs; a++) {
+    if (
+      a < numArgs - 1 && (
+        0 == strcasecmp(args[a], "--delim") ||
+        0 == strcasecmp(args[a], "-d")
+      )
+    ) {
+      delim = args[++a][0];
+    } else if (
+      a < numArgs - 1 && (
+        0 == strcasecmp(args[a], "--input") ||
+        0 == strcasecmp(args[a], "-i")
+      )
+    ) {
+      fname = args[++a];
+    } else {
+      std::cout << "Syntax: [(--delim|-d) DELIM] [(--input|-i) FILE]" << std::endl;
+      exit(1);
+    }
+  }
+
+  std::string input { readWholeFile(fname) };
+  Pointer src(input);
+
+  while (src.rem() > 0) {
+    /* Accumulate that input into the global state: */
+    src = dessser_gen::%s(src);
+  }
+
+  /* Output the finalized state: */
+  Pointer dst { %d };
+  dst = dessser_gen::%s(dst);
+  assert(dst.offset < dst.size-1);
+
+  if (dst.buffer) {
+    fwrite(dst.buffer.get(), 1, dst.offset, stdout);
+    if (delim != '\0') fwrite(&delim, sizeof(delim), 1, stdout);
+  } // else it's a heap value
+
+  return 0;
+}
+|} input_name out_buf_size output_name
