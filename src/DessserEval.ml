0d9e052ac3db43ad74317a7982f247b1affa0864
Cedric Cellier <rixed@happyleptic.org>
2021-07-19 06:51:34 +0200

    peval: optimize away `ptr-add _ 0`

--- src/DessserEval.ml
+++ src/DessserEval.ml
@@ -1052,6 +1052,8 @@ let rec peval l e =
           E.E2 (PtrAdd, e1_1, E.E2 (Add, e1_2, e2)) |>
           replace_final_expression_anonymously e1 |>
           repl
+      | PtrAdd, _, E0 (Size 0) ->
+          replace_final_expression_anonymously e2 e1
       | BitAnd, f1, f2 ->
           (try arith2 f1 f2 no_floats Int128.logand Uint128.logand |> repl
           with Invalid_argument _ -> E.E2 (BitAnd, e1, e2))
