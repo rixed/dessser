f1346f840926193e3dbbacc39795b30ecdc79c00
Cedric Cellier <rixed@happyleptic.org>
2022-04-23 11:44:41 +0200

    RingBuf: Rename fieldmask into bitmask to avoid confusion with the runtime fieldmask

--- src/DessserRamenRingBuffer.ml
+++ src/DessserRamenRingBuffer.ml
@@ -149,10 +149,10 @@ let align_const p extra_bytes =
 let tuple_typs_of_record mns =
   Array.map snd mns
 
-(* We use a stack of "frames" of pointer to fieldmask + fieldbit index, where
- * the fieldmask points at a bitmask with one bit per field, set if the field
+(* We use a stack of "frames" of pointer to bitmask + fieldbit index, where
+ * the bitmask points at a bitmask with one bit per field, set if the field
  * value is actually present (non default).
- * Every fieldmask starts with a byte giving ther length in words of this
+ * Every bitmask starts with a byte giving ther length in words of this
  * bitmask (although it is only really necessary for arrays).
  * Our "data pointer" is therefore actually composed of the data pointer
  * itself (p) and a stack (stk): *)
@@ -162,7 +162,7 @@ let leave_frame p_stk =
   E.with_sploded_pair "leave_frame" p_stk (fun p stk ->
     make_pair p (force ~what:"RingBuf.leave_frame" (tail stk)))
 
-(* Set the next bit in the fieldmask and return the new stack with increased
+(* Set the next bit in the bitmask and return the new stack with increased
  * fieldbit position: *)
 let set_fieldbit stk =
   let p_bi = force ~what:"RingBuf.set_fieldbit" (head stk) in
@@ -174,8 +174,8 @@ let set_fieldbit stk =
           let frame = make_pair p (add bi (size 1)) in
           cons frame (force ~what:"RingBuf.set_fieldbit2" (tail stk)) ])
 
-(* Skip a bit in the fieldmask, ie just increment the fieldbit (since the
- * fieldmask is initialized to zero), and return as above the stack with
+(* Skip a bit in the bitmask, ie just increment the fieldbit (since the
+ * bitmask is initialized to zero), and return as above the stack with
  * that new fieldbit. *)
 let skip_fieldbit stk =
   let p_bi = force ~what:"RingBuf.skip_fieldbit" (head stk) in
@@ -194,7 +194,7 @@ let skip_fieldbit_from_frame p_stk =
     let stk = skip_fieldbit stk in
     make_pair p stk)
 
-module FieldMaskWidth =
+module BitMaskWidth =
 struct
   let tup_bits mns =
     Array.length mns
@@ -212,8 +212,8 @@ struct
     | T.TVec (dim, _) ->
         vec_bits dim
     | TArr _ ->
-        (* Arrays fieldmask is dynamic and prefixed with a length *)
-        invalid_arg "FieldMaskWidth.of_type for lists"
+        (* Arrays bitmask is dynamic and prefixed with a length *)
+        invalid_arg "BitMaskWidth.of_type for lists"
     | TTup mns ->
         tup_bits mns
     | TRec mns ->
@@ -225,12 +225,12 @@ struct
     | _ ->
         0
 
-  (* Return the number of bytes required to encode the fieldmask *)
+  (* Return the number of bytes required to encode the bitmask *)
   let bytes_of_type typ =
     let bits = of_type typ in
     round_up_const_bits (bits + 8) (* with prefix length *)
 
-  (* Return the number of words required to encode the fieldmask *)
+  (* Return the number of words required to encode the bitmask *)
   let words_of_type =
     words_of_const_bytes % bytes_of_type
 end
@@ -244,31 +244,31 @@ struct
 
   (* Few helper functions: *)
 
-  (* Zero a fieldmask which width is known at compile time, and advance the
+  (* Zero a bitmask which width is known at compile time, and advance the
    * pointer. *)
-  let zero_fieldmask_const bits p =
+  let zero_bitmask_const bits p =
     let sz = (bits + 7) / 8 in
     let words = words_of_const_bytes (sz + 1) in
     let p = write_u8 p (u8_of_int words) in
     let p = blit_byte p (u8_of_int 0) (size sz) in
     align_const p (1 + sz)
 
-  (* Zero the fieldmask known only at runtime (which size in words a given
+  (* Zero the bitmask known only at runtime (which size in words a given
    * in the first byte of the mask) and advance the pointer *)
-  let zero_fieldmask_dyn bits p =
+  let zero_bitmask_dyn bits p =
     let_ ~name:"sz_" (right_shift (add (u32_of_int 7) bits) (u8_of_int 3))
       (fun sz ->
-        (* The fieldmask is prefixed with a byte for the length (in words): *)
+        (* The bitmask is prefixed with a byte for the length (in words): *)
         let_ ~name:"sz_pfx_" (add sz (u32_of_int 1)) (fun sz_with_prefix ->
           let words = words_of_dyn_bytes sz_with_prefix in
           let p = write_u8 p (to_u8 words) in
           let p = blit_byte p (u8_of_int 0) (size_of_u32 sz) in
           align_dyn p (size_of_u32 sz_with_prefix)))
 
-  (* Enter a new compound type by zeroing a fieldmask of the given [max_width]
+  (* Enter a new compound type by zeroing a bitmask of the given [max_width]
    * and setting up a new frame for it, all this after having set its own
    * fieldbit. *)
-  let enter_frame to_expr zero_fieldmask max_width p_stk =
+  let enter_frame to_expr zero_bitmask max_width p_stk =
     E.with_sploded_pair "enter_frame" p_stk (fun p stk ->
       let stk = set_fieldbit_to true stk in
       let new_frame = make_pair p (size 8 (* width of the length prefix *)) in
@@ -278,13 +278,13 @@ struct
             debug (string_of_int_ (to_expr max_width)) ;
             debug (string " fieldbits\n") ;
             make_pair
-              (zero_fieldmask max_width p)
+              (zero_bitmask max_width p)
               (cons new_frame stk) ])
 
-  (* Enter a new compound type by zeroing a fieldmask and setting up a new
+  (* Enter a new compound type by zeroing a bitmask and setting up a new
    * frame for it, all this after having set its own fieldbit: *)
-  let enter_frame_dyn = enter_frame identity zero_fieldmask_dyn
-  let enter_frame_const = enter_frame u8_of_int zero_fieldmask_const
+  let enter_frame_dyn = enter_frame identity zero_bitmask_dyn
+  let enter_frame_const = enter_frame u8_of_int zero_bitmask_const
 
   let with_fieldbit_done p_stk f =
     E.with_sploded_pair "with_fieldbit_done1" p_stk (fun p stk ->
@@ -299,7 +299,7 @@ struct
   let start _conf p =
     let stk = end_of_list t_frame in
     let new_frame = make_pair p (size 8 (* width of the length prefix *)) in
-    make_pair (zero_fieldmask_const 1 p) (cons new_frame stk)
+    make_pair (zero_bitmask_const 1 p) (cons new_frame stk)
 
   let stop () p_stk =
     (* TODO: assert tail stk = end_of_list *)
@@ -427,7 +427,7 @@ struct
 
   let tup_rec_opn mns _ _ p_stk =
     (* Allocate one bit per item. *)
-    let bits = FieldMaskWidth.tup_bits mns in
+    let bits = BitMaskWidth.tup_bits mns in
     enter_frame_const bits p_stk
 
   let tup_opn mns () mn0 path p_stk =
@@ -448,7 +448,7 @@ struct
   let rec_sep () _ _ p_stk = p_stk
 
   (* Sum types are encoded with a 1-dword header composed of:
-   * - a 16bits fieldmask, of which only bit 0 will ever be used
+   * - a 16bits bitmask, of which only bit 0 will ever be used
    * - the u16 of the label index. *)
   let sum_opn _mns lbl () _ _ p_stk =
     E.with_sploded_pair "sum_opn1" p_stk (fun p stk ->
@@ -457,7 +457,7 @@ struct
       (* Prepare the new frame: *)
       let new_frame = make_pair p (size 0) in
       let stk = cons new_frame stk in
-      (* And zero that fieldmask: *)
+      (* And zero that bitmask: *)
       let p = write_u16 LittleEndian p (u16_of_int 0) in
       (* Then the label index: *)
       let p = write_u16 LittleEndian p lbl in
@@ -468,7 +468,7 @@ struct
     leave_frame p_stk
 
   let vec_opn dim _mn () _ _ p_stk =
-    let bits = FieldMaskWidth.vec_bits dim in
+    let bits = BitMaskWidth.vec_bits dim in
     enter_frame_const bits p_stk
 
   let vec_cls () _ _ p_stk =
@@ -481,7 +481,7 @@ struct
     let n = match n with
       | Some n -> n
       | None -> failwith "RamenRingBuffer.Ser needs list size upfront" in
-    let bits = FieldMaskWidth.lst_bits n in
+    let bits = BitMaskWidth.lst_bits n in
     let p_stk =
       E.with_sploded_pair "with_data_ptr" p_stk (fun p stk ->
         let p = write_u32 LittleEndian p n in
@@ -495,7 +495,7 @@ struct
 
   let nullable () _ _ p_stk = p_stk
 
-  (* The fieldmask has been zeroed already: *)
+  (* The bitmask has been zeroed already: *)
   let snull _t () _ _ p_stk =
     skip_fieldbit_from_frame p_stk
 
@@ -518,13 +518,13 @@ struct
 
   (* SerSize of the list header: *)
   let ssize_of_arr _ _ id =
-    let fieldmask_bits_dyn = cardinality id in
-    (* Add the fieldmask length prefix: *)
-    let fieldmask_sz_bits = add fieldmask_bits_dyn (u32_of_int 8) in
+    let bitmask_bits_dyn = cardinality id in
+    (* Add the bitmask length prefix: *)
+    let bitmask_sz_bits = add bitmask_bits_dyn (u32_of_int 8) in
     (* Round up to ringbuf words: *)
-    let fieldmask_bytes = round_up_dyn_bits fieldmask_sz_bits in
+    let bitmask_bytes = round_up_dyn_bits bitmask_sz_bits in
     add (size word_size) (* list length *)
-        fieldmask_bytes
+        bitmask_bytes
 
   let ssize_of_float _ _ _ =
     size (round_up_const_bytes 8)
@@ -591,24 +591,24 @@ struct
 
   let ssize_of_tup mn0 path  _ =
     (* Just the additional bitmask: *)
-    let fieldmask_words =
-      FieldMaskWidth.words_of_type (Path.type_of_path mn0 path).typ in
-    size (fieldmask_words * word_size)
+    let bitmask_words =
+      BitMaskWidth.words_of_type (Path.type_of_path mn0 path).typ in
+    size (bitmask_words * word_size)
 
   let ssize_of_rec mn0 path _ =
     (* Just the additional bitmask: *)
-    let fieldmask_words =
-      FieldMaskWidth.words_of_type (Path.type_of_path mn0 path).typ in
-    size (fieldmask_words * word_size)
+    let bitmask_words =
+      BitMaskWidth.words_of_type (Path.type_of_path mn0 path).typ in
+    size (bitmask_words * word_size)
 
   (* Just the additional label: *)
   let ssize_of_sum _ _ _ =
     size word_size
 
   let ssize_of_vec mn0 path _ =
-    let fieldmask_words =
-      FieldMaskWidth.words_of_type (Path.type_of_path mn0 path).typ in
-    size (fieldmask_words * word_size)
+    let bitmask_words =
+      BitMaskWidth.words_of_type (Path.type_of_path mn0 path).typ in
+    size (bitmask_words * word_size)
 
   let ssize_of_null _ _ = size 0
 
@@ -629,7 +629,7 @@ struct
   (* Enter a new compound type by recording its location in the stack
    * and jumping over it, after having incremented the current fieldbit
    * index.
-   * The fieldbit index of the new stack is set to 8 after the fieldmask
+   * The fieldbit index of the new stack is set to 8 after the bitmask
    * length prefix (so sum deserializer cannot use this function) *)
   let enter_frame p stk =
     let stk = set_fieldbit_to false stk in
@@ -827,7 +827,7 @@ struct
 
   let vec_sep () _ _ p_stk = p_stk
 
-  (* Sums are encoded with a leading u16 for the fieldmask followed by
+  (* Sums are encoded with a leading u16 for the bitmask followed by
    * the label as a u16: *)
   let sum_opn _mns () _ _ p_stk =
     E.with_sploded_pair "sum_opn2" p_stk (fun p stk ->
@@ -836,7 +836,7 @@ struct
       (* Prepare the new frame: *)
       let new_frame = make_pair p (size 0) in
       let stk = cons new_frame stk in
-      (* Skip that fieldmask: *)
+      (* Skip that bitmask: *)
       let p = ptr_add p (size 2) in
       (* Read the label: *)
       let w_p = read_u16 LittleEndian p in
